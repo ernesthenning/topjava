a40c2716ab985a0579b04abcb20686bb160029fb
Vladislav Malinnikov <vladmalff@gmail.com>
2022-10-25 17:08:17 +0300

    fixed test

--- src/test/java/ru/javawebinar/topjava/web/user/InMemoryAdminRestControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/user/InMemoryAdminRestControllerTest.java
@@ -1,11 +1,16 @@
 package ru.javawebinar.topjava.web.user;
 
 import org.junit.*;
+import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 import ru.javawebinar.topjava.repository.inmemory.InMemoryUserRepository;
+import ru.javawebinar.topjava.service.UserService;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
 import java.util.Arrays;
@@ -13,26 +18,30 @@ import java.util.Arrays;
 import static ru.javawebinar.topjava.UserTestData.NOT_FOUND;
 import static ru.javawebinar.topjava.UserTestData.USER_ID;
 
-@Ignore
+@ContextConfiguration("classpath:spring-inmemory-test.xml")
 public class InMemoryAdminRestControllerTest {
     private static final Logger log = LoggerFactory.getLogger(InMemoryAdminRestControllerTest.class);
 
     private static ConfigurableApplicationContext appCtx;
+
     private static AdminRestController controller;
-    private static InMemoryUserRepository repository;
 
-    @BeforeClass
-    public static void beforeClass() {
-        appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml");
-        log.info("\n{}\n", Arrays.toString(appCtx.getBeanDefinitionNames()));
-        controller = appCtx.getBean(AdminRestController.class);
-        repository = appCtx.getBean(InMemoryUserRepository.class);
-    }
+    private  static InMemoryUserRepository repository;
 
-    @AfterClass
-    public static void afterClass() {
-        appCtx.close();
-    }
+
+        @BeforeClass
+        public static void beforeClass() {
+            appCtx = new ClassPathXmlApplicationContext("classpath:spring-inmemory-test.xml");
+            log.info("\n{}\n", Arrays.toString(appCtx.getBeanDefinitionNames()));
+            controller = appCtx.getBean(AdminRestController.class);
+            UserService service = appCtx.getBean(UserService.class);
+            repository = appCtx.getBean(InMemoryUserRepository.class);
+        }
+
+        @AfterClass
+        public static void afterClass() {
+            appCtx.close();
+        }
 
     @Before
     public void setUp() {
