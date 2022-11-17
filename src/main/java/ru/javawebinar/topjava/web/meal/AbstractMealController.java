fe4ab04c7d10c86731779f80e480ab1716fafcab
Vladislav Malinnikov <vladmalff@gmail.com>
2022-11-17 07:13:21 +0300

    HW06 basic done

--- src/main/java/ru/javawebinar/topjava/web/meal/AbstractMealController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/AbstractMealController.java
@@ -20,44 +20,44 @@ import static ru.javawebinar.topjava.util.ValidationUtil.checkNew;
 @Controller
 public abstract class AbstractMealController {
 
-    private static final Logger log = LoggerFactory.getLogger(MealRestController.class);
+    protected static final Logger log = LoggerFactory.getLogger(MealRestController.class);
 
-    private final MealService service;
+    protected final MealService mealService;
 
-    public AbstractMealController(MealService service) {
-        this.service = service;
+    public AbstractMealController(MealService mealService) {
+        this.mealService = mealService;
     }
 
     public Meal get(int id) {
         int userId = SecurityUtil.authUserId();
         log.info("get meal {} for user {}", id, userId);
-        return service.get(id, userId);
+        return mealService.get(id, userId);
     }
 
     public void delete(int id) {
         int userId = SecurityUtil.authUserId();
         log.info("delete meal {} for user {}", id, userId);
-        service.delete(id, userId);
+        mealService.delete(id, userId);
     }
 
     public List<MealTo> getAll() {
         int userId = SecurityUtil.authUserId();
         log.info("getAll for user {}", userId);
-        return MealsUtil.getTos(service.getAll(userId), SecurityUtil.authUserCaloriesPerDay());
+        return MealsUtil.getTos(mealService.getAll(userId), SecurityUtil.authUserCaloriesPerDay());
     }
 
     public Meal create(Meal meal) {
         int userId = SecurityUtil.authUserId();
         checkNew(meal);
         log.info("create {} for user {}", meal, userId);
-        return service.create(meal, userId);
+        return mealService.create(meal, userId);
     }
 
     public void update(Meal meal, int id) {
         int userId = SecurityUtil.authUserId();
         assureIdConsistent(meal, id);
         log.info("update {} for user {}", meal, userId);
-        service.update(meal, userId);
+        mealService.update(meal, userId);
     }
 
     /**
@@ -71,7 +71,7 @@ public abstract class AbstractMealController {
         int userId = SecurityUtil.authUserId();
         log.info("getBetween dates({} - {}) time({} - {}) for user {}", startDate, endDate, startTime, endTime, userId);
 
-        List<Meal> mealsDateFiltered = service.getBetweenInclusive(startDate, endDate, userId);
+        List<Meal> mealsDateFiltered = mealService.getBetweenInclusive(startDate, endDate, userId);
         return MealsUtil.getFilteredTos(mealsDateFiltered, SecurityUtil.authUserCaloriesPerDay(), startTime, endTime);
     }
 }
--- src/main/java/ru/javawebinar/topjava/web/meal/JspMealController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/JspMealController.java
@@ -1,19 +1,12 @@
 package ru.javawebinar.topjava.web.meal;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
-import ru.javawebinar.topjava.service.UserService;
-import ru.javawebinar.topjava.to.MealTo;
-import ru.javawebinar.topjava.util.MealsUtil;
 import ru.javawebinar.topjava.web.SecurityUtil;
 
 import javax.servlet.http.HttpServletRequest;
@@ -21,83 +14,64 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
-import java.util.List;
 
 import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalDate;
 import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalTime;
-import static ru.javawebinar.topjava.util.ValidationUtil.assureIdConsistent;
-import static ru.javawebinar.topjava.util.ValidationUtil.checkNew;
 
 @Controller
-public class JspMealController {
-    private static final Logger log = LoggerFactory.getLogger(JspMealController.class);
+public class JspMealController extends AbstractMealController {
 
-    @Autowired
-    private MealService mealService;
-
-    @Autowired
-    private UserService userService;
+    public JspMealController(MealService mealService) {
+        super(mealService);
+    }
 
     @GetMapping("/meals")
     public String getAll(Model model) {
-        int userId = SecurityUtil.authUserId();
-        List<Meal> meals = mealService.getAll(userId);
-        List<MealTo> mealTos = MealsUtil.getTos(meals, userService.get(userId).getCaloriesPerDay());
-        model.addAttribute("meals", mealTos);
-        log.info("get all meals for user {}", userId);
+        model.addAttribute("meals", getAll());
         return "meals";
     }
 
     @GetMapping("/filter")
-    public String filter(Model model, HttpServletRequest request) {
+    public String filter(HttpServletRequest request) {
         LocalDate startDate = parseLocalDate(request.getParameter("startDate"));
         LocalDate endDate = parseLocalDate(request.getParameter("endDate"));
         LocalTime startTime = parseLocalTime(request.getParameter("startTime"));
         LocalTime endTime = parseLocalTime(request.getParameter("endTime"));
-        int userId = SecurityUtil.authUserId();
-        model.addAttribute("meals", MealsUtil.getFilteredTos(mealService.getBetweenInclusive(startDate, endDate, userId),
-                userService.get(userId).getCaloriesPerDay(), startTime, endTime));
-        log.info("get between dates {} - {} and times {} - {} for user {}", startDate, endDate, startTime, endTime, userId);
+        getBetween(startDate, startTime, endDate, endTime);
         return "meals";
     }
 
     @GetMapping("/delete")
     public String delete(@RequestParam String id) {
         int mealId = Integer.parseInt(id);
-        int userId = SecurityUtil.authUserId();
-        mealService.delete(mealId, userId);
-        log.info("delete meal {} for user {}", id, userId);
+        delete(mealId);
         return "redirect:meals";
     }
 
-    @GetMapping("/update")
-    public String updateGet(Model model, @RequestParam String id) {
-        int mealId = Integer.parseInt(id);
-        model.addAttribute("meal", mealService.get(mealId, SecurityUtil.authUserId()));
-        return "mealForm";
-    }
-
-    @GetMapping("/create")
-    public String createGet(Model model) {
-        model.addAttribute("meal", new Meal(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "", 1000));
+    @GetMapping("/save")
+    public String save(Model model, HttpServletRequest request) {
+        if (request.getParameter("action").equals("create")) {
+            model.addAttribute("meal",
+                    new Meal(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "", 1000));
+        } else {
+            String id = request.getParameter("id");
+            int mealId = Integer.parseInt(id);
+            model.addAttribute("meal", mealService.get(mealId, SecurityUtil.authUserId()));
+        }
         return "mealForm";
     }
 
     @PostMapping("/save")
     public String save(HttpServletRequest request) {
-        int userId = SecurityUtil.authUserId();
+        String id = request.getParameter("id");
         Meal meal = new Meal(
                 LocalDateTime.parse(request.getParameter("dateTime")),
                 request.getParameter("description"),
                 Integer.parseInt(request.getParameter("calories")));
-
-        if (!StringUtils.hasLength(request.getParameter("id"))) {
-            checkNew(meal);
-            mealService.update(meal, userId);
+        if (!id.isEmpty()) {
+            update(meal, Integer.parseInt(id));
         } else {
-            int id = Integer.parseInt(request.getParameter("id"));
-            assureIdConsistent(meal, id);
-            mealService.create(meal, userId);
+            create(meal);
         }
         return "redirect:meals";
     }
